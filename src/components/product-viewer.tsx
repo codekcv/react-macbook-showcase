1af93c156a331b683f0fe565947ef5184e6fa3cb
codekcv <christianvillamin31@gmail.com>
2025-10-11 22:44:17 +0800

    feat: three.js, init canvas/control

--- src/components/product-viewer.tsx
+++ src/components/product-viewer.tsx
@@ -1,5 +1,7 @@
 import clsx from "clsx";
 import useMacbookStore from "../store";
+import { Canvas } from "@react-three/fiber";
+import { Box, OrbitControls } from "@react-three/drei";
 
 export function ProductViewer() {
   const { color, scale, setColor, setScale } = useMacbookStore();
@@ -58,7 +60,13 @@ export function ProductViewer() {
         </div>
       </div>
 
-      <p className="text-white text-4xl">Render Canvas</p>
+      <Canvas
+        id="canvas"
+        camera={{ position: [0, 2, 5], fov: 50, near: 0.1, far: 100 }}
+      >
+        <Box position={[0, 0, 0]} scale={10 * scale} material-color={color} />
+        <OrbitControls enableZoom={false} />
+      </Canvas>
     </section>
   );
 }
