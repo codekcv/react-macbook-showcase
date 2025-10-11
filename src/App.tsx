07611f5422b3af65a7f40d333b68f27c116baaff
codekcv <christianvillamin31@gmail.com>
2025-10-11 22:37:44 +0800

    feat: init product viewer, zustand

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Hero } from "./components/hero";
 import { Navbar } from "./components/navbar";
+import { ProductViewer } from "./components/product-viewer";
 
 export function App() {
   return (
     <main>
       <Navbar />
       <Hero />
+      <ProductViewer />
     </main>
   );
 }
